6d540941cad803dee9b18a9c554868ccf407fcc9
Igues de Lacerda <igueslacerda@gmail.com>
2024-02-06 22:47:44 -0300

    Incrementações em index.js para exercício de JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myFirstElement = <h1>Rélou Uordi<span style={{color: 'red'}}>!</span></h1>
+const content = (<>
+<h1>Rélou Uordi<span style={{color: 'red'}}>!</span></h1>
+<table>
+    <tr><th>id</th><th>Nome</th><th>Letra</th></tr>
+    <tr><td>1</td><td>Igues</td><td>A</td></tr>
+    <tr><td>2</td><td>Tiago</td><td>B</td></tr>
+</table>
+</>);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(content);

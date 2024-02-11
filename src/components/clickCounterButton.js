c86501a03163773125a4944c20041584b86cc985
Igues de Lacerda <igueslacerda@gmail.com>
2024-02-11 19:46:34 -0300

    Estudo de componente de classe com state.

---
+++ src/components/clickCounterButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from "./card";
+
+export default class ClickCounterButton extends React.Component {
+    constructor() {
+        super();
+        this.state = { clicks: 0,};
+    }
+
+    clickAndCount = () => {
+        let clicks = this.state.clicks;
+        this.setState({ clicks: clicks + 1 });
+    };
+
+    render() {
+        return Card(<>
+            <p>Times clicked: {this.state.clicks}</p>
+            <button type='button' onClick={this.clickAndCount}>Click to add +1</button>
+        </>);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Card from './components/card';
+import ClickCounterButton from './components/clickCounterButton';
 
-const content = Card(<>
+const card1 = Card(<>
 <h1>Rélou Uordi<span style={{color: 'red'}}>!</span></h1>
 <table>
     <tr><th>id</th><th>Nome</th><th>Letra</th></tr>
@@ -10,6 +11,9 @@ const content = Card(<>
     <tr><td>2</td><td>Tiago</td><td>B</td></tr>
 </table>
 </>);
+
+const content = <>{card1}<ClickCounterButton/></>
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(content);
